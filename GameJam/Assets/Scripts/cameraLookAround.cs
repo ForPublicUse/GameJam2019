254b1b18d3e08cee813d4b702e92f37501a10c2f
LorenLee <1606598659@qq.com>
2019-12-14 19:55:28 +0800

    newLch

--- GameJam/Assets/Scripts/cameraLookAround.cs
+++ GameJam/Assets/Scripts/cameraLookAround.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cameraControl : MonoBehaviour
+public class cameraLookAround : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
